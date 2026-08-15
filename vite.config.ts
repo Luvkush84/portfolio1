e6f6641f4dca96fd7065da7f400a722ef6a10056
Luvkush84 <lovekushsaini033@gmail.com>
2026-08-15 16:24:15 +0530

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/portfolio1/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
